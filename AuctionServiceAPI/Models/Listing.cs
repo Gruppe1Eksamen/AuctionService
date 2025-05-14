14f18bb205a84c9b7c905a33bb23efcceae8b2d4
vincentlorenzen <vincent.lorenzen@gmail.com>
2025-05-14 10:14:05 +0200

    start

---
+++ AuctionServiceAPI/Models/Listing.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace DefaultNamespace;
+
+public enum ListingCategory
+{
+    None = 0,
+    Furniture = 1,
+    Jewellery = 2,
+    Porcelain = 3,
+    Gold = 4,
+    Silver = 5,
+    Painting = 6
+}
+
+public class Listing
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.String)]
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public float AssesedPrice { get; set; }
+    public string Description { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+
+    public ListingCategory ListingCategory { get; set; }
+    public List<Uri> Image { get; set; } = new List<Uri>();
+}
---
+++ AuctionServiceAPI/Services/MongoDBContext.cs
@@ -0,0 +1,19 @@
+﻿namespace DefaultNamespace;
+
+public class MongoDBContext
+{
+    public IMongoDatabase Database { get; set; }
+    public IMongoCollection<Auction> Collection { get; set; }
+    
+    public MongoDBContext(ILogger<AuctionMongoDBService> logger, IConfiguration config)
+    {        
+        BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+
+        var client = new MongoClient(config["MongoConnectionString"]);
+        Database = client.GetDatabase(config["AuctionDB"]);
+        Collection = Database.GetCollection<Listing>(config["Auctions"]);
+
+        logger.LogInformation($"Connected to database {config["AuctionDB"]}");
+        logger.LogInformation($"Using collection {config["Auctions"]}");
+    }
+}
