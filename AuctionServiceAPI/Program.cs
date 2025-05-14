5ad058074c9f147e3e5ab90f2c579dcc6af7e097
vincentlorenzen <vincent.lorenzen@gmail.com>
2025-05-14 10:15:57 +0200

    initialcommit

---
+++ AuctionServiceAPI/Program.cs
@@ -0,0 +1,28 @@
+using AuctionService.Models
+using AuctionService.Services
+    using MongoDB.Driver
+    using System.Text.Json.Serialization
+    
+    
+    
+    
+    
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+
+app.Run();
+
+
